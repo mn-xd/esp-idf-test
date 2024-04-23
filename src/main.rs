1fb6929c0f96fa04e79e8fe1b8af5be6d9f114bc
mnxd <mnxd.git@gmail.com>
2024-04-23 12:42:21 +0200

    Removed comments

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,5 @@
 use embedded_storage::{self, ReadStorage};
 use esp_storage::FlashStorage;
-// use esp_idf_svc::{
-//     self,
-//     hal::{delay::FreeRtos, gpio::PinDriver, peripherals::Peripherals},
-// };
 
 fn main() {
     // It is necessary to call this function once. Otherwise some patches to the runtime
@@ -15,17 +11,4 @@ fn main() {
     // let peripherals = Peripherals::take().unwrap();
     let flash = FlashStorage::new();
     log::info!("Flsh capacity: {}", flash.capacity());
-
-    // let button_1 = PinDriver::input(peripherals.pins.gpio32).unwrap();
-    // let mut button_2 = PinDriver::input(peripherals.pins.gpio33).unwrap();
-
-    // loop {
-    //     if button_1.is_high() {
-    //         log::info!("Button 1 pressed on pin {}", button_1.pin());
-    //     }
-    //     if button_2.is_high() {
-    //         log::info!("Button 2 pressed on pin {}", button_2.pin());
-    //     }
-    //     FreeRtos::delay_ms(20);
-    // }
 }
